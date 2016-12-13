49ee2de31089b794b952efe427d45e1296636832
Stephen Harris <smhmic@gmail.com>
2016-12-13 10:18:43 -0500

    add ga-spy-log bookmarklet

---
+++ examples/ga-spy-log.bookmarklet.js
@@ -0,0 +1,3 @@
+(function(){
+  document.body.appendChild(document.createElement('script')).src='https://cdn.rawgit.com/smhmic/ga-spy/14c5e5f6027ce32c6496c012fa01550e30e0957a/examples/ga-spy-log.js';
+})();
